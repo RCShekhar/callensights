e46038a9c9cda822647218de36056d9841db3e61
Chandrashekhar Rachakonda <shekhar.rachakonda@gmail.com>
2023-05-27 20:23:21 +0530

    updated code for aws

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, redirect
-from google.cloud import storage
+from flask import Flask, render_template, request, jsonify
+import boto3
 
 app = Flask(__name__)
 
-# Configure Google Cloud Storage
-storage_client = storage.Client()
+# Configure AWS S3
+s3_client = boto3.client('s3')
 
 @app.route('/')
 def home():
@@ -14,20 +14,18 @@ def home():
 def upload():
     # Check if a file was uploaded
     if 'file' not in request.files:
-        return redirect('/')
+        return jsonify({'error': 'No file uploaded'})
 
     file = request.files['file']
 
     # Check if file has a name
     if file.filename == '':
-        return redirect('/')
+        return jsonify({'error': 'No file selected'})
 
-    # Upload file to Google Cloud Storage
-    bucket = storage_client.get_bucket('salesmaster')
-    blob = bucket.blob(file.filename)
-    blob.upload_from_file(file)
+    # Upload file to S3
+    s3_client.upload_fileobj(file, 'your-s3-bucket-name', file.filename)
 
-    return render_template('success.html')
+    return jsonify({'message': 'Upload successful'})
 
 if __name__ == '__main__':
     app.run(debug=True)
